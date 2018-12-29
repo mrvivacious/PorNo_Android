6dc50a16a74231495ab1021a9a0702f9a6bb05da
Vivek Bhookya <v.bhookya98@gmail.com>
2018-12-28 20:03:25 -0600

    We have PorNo! into about:blank. Next, use the redirect links and speed it tf up

--- app/src/main/java/us/mrvivacio/porno/MyAccessibilityService.java
+++ app/src/main/java/us/mrvivacio/porno/MyAccessibilityService.java
@@ -61,96 +61,54 @@ public class MyAccessibilityService extends AccessibilityService {
                     // Save cycles by DFSing only on the events directly from the omnibox
                     if (nodeInfo.contains("@80006")) {
                         Log.d(TAG, "onAccessibilityEvent: src.NodeInfo = " + nodeInfo);
+
+                        // Can't use .getText() strategy -- WINDOW event metadata doesn't contain URL info
+                        //  when opened through hyperlinks therefore dfs() we go
                         dfs(src);
                     }
                 }
             }
             // If the user is typing in the omnibox,
-            else if (eventType.contains("TEXT")) {
-                AccessibilityNodeInfo src = event.getSource();
-
-                // Nothin 2 do
-                if (src == null) {
-                    // Do nothing
-                }
-                // We have a source, so we can query
-                else {
-                    // Get the nodeInfo of the sender of this event
-                    String nodeInfo = src.toString().substring(48, src.toString().indexOf(';'));
-
-                    // The nodeId of the Chrome omnibox seems to begin with this for TEXT events
-                    // Save cycles by DFSing only on the events directly from the omnibox
-                    if (nodeInfo.contains("@75")) {
-                        Log.d(TAG, "onAccessibilityEvent TEXT__: src.NodeInfo = " + nodeInfo);
-                        Log.d(TAG, "onAccessibilityEvent: TEXT__: event.getTxt = " + event.getText());
-
-                        if (event.getText().toString().contains("yahoo.com")) {
-                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("about:blank"));
-                            intent.putExtra(Browser.EXTRA_APPLICATION_ID, "com.android.chrome");
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intent);
-
-                            Log.d(TAG, "dfs: Speed = " + (System.currentTimeMillis() - time));
-                        }
-//                        dfs(src);
-                    }
-                }
-            }
+            // TODO: Add handling to ensure the URL found is the omnibox...the resource id keeps changing UGHHHHHHHH
+//            else if (eventType.contains("TEXT")) {
+//                AccessibilityNodeInfo src = event.getSource();
+//
+//                // Nothin 2 do
+//                if (src == null) {
+//                    // Do nothing
+//                }
+//                // We have a source, so we can query
+//                else {
+//                    // Get the nodeInfo of the sender of this event
+//                    String nodeInfo = src.toString().substring(48, src.toString().indexOf(';'));
+//
+//                    // The nodeId of the Chrome omnibox seems to begin with this for TEXT events
+//                    // Save cycles by DFSing only on the events directly from the omnibox
+////                    if (nodeInfo.contains("@75")) {
+//                        Log.d(TAG, "onAccessibilityEvent TEXT__: src.NodeInfo = " + nodeInfo);
+//                        Log.d(TAG, "onAccessibilityEvent: TEXT__: event.getTxt = " + event.getText());
+//
+//                        if (event.getText().toString().contains("yahoo.com")) {
+//                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("about:blank"));
+//                            intent.putExtra(Browser.EXTRA_APPLICATION_ID, "com.android.chrome");
+//                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                            startActivity(intent);
+//
+//                            Log.d(TAG, "dfs: Speed = " + (System.currentTimeMillis() - time));
+////                        }
+////                        dfs(src);
+//                    }
+//                }
+//            }
         }
 
 
-        // The following statements perform URL detection ONLY when the user is TYPING the
-        //  URL into the omnibox
-        // How to detect that we have visited a URL from a hyperlink
-        // Todo: typing the url will redirect, but opening the url from a hyperlink does not trigger!
-//        if (eventType.contains("VIEW_TEXT")) {
-////            Log.d(TAG, "onAccessibilityEvent: event = " + event);
-////            Log.d(TAG, "onAccessibilityEvent: event.PackageName = " + event.getPackageName());
-////            Log.d(TAG, "onAccessibilityEvent: event.Text = " + event.getText());
-//
-//            // User is using Google Chrome
-//            if (event.getPackageName().toString().contains("com.android.chrome")) {
-////                Log.d(TAG, "onAccessibilityEvent: We are inside chrome");
-////                Log.d(TAG, "onAccessibilityEvent: eventType = " + event);
-//
-//                String eventText = event.getText().toString();
-//                // Search for url as quickly as possible
-//                // !! This will evaluate to true even if the user just types a URL
-//                //  in any text field beyond the omnibox...
-//                // Todo: How to parse strictly just the omnibox?
-////                if (eventText.contains("https://") || eventText.contains("http://")) {
-//                    Log.d(TAG, "onAccessibilityEvent: we have a url: " + eventText);
-//                    // Is the url banned (ie equals yahoo.com)?
-//                    if (eventText.contains("yahoo.com")) {
-//                        // If the url is banned,
-//                        // Redirect the user
-////                        Log.d(TAG, "onAccessibilityEvent: event.getSource = " + event.getSource().getViewIdResourceName().toString());
-//
-//                        long old = System.currentTimeMillis();
-////                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("about:blank"));
-//                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/mrvivacious"));
-//                        intent.putExtra(Browser.EXTRA_APPLICATION_ID, "com.android.chrome");
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                        startActivity(intent);
-//
-//                        Log.d(TAG, "onAccessibilityEvent: Speed = " + (System.currentTimeMillis() - old));
-////                    }
-//                }
-//            }
 //
 //            // User is using Samsung Internet
 //            else if (event.getPackageName().toString().contains("com.sec.android.app.sbrowser")) {
 //                // implement()
 //            }
-//        }
 
-//        if (eventType.contains("WINDOW")) {
-//            Log.d(TAG, "onAccessibilityEvent: CONTAINS WINDOWWWWWW");
-//
-//            AccessibilityNodeInfo nodeInfo = event.getSource();
-////            Log.d(TAG, "onAccessibilityEvent: info.getChildCount() = " + nodeInfo.getChildCount());
-////            dfs(nodeInfo);
-//        }
     }
 
     public void dfs(AccessibilityNodeInfo info){
@@ -162,17 +120,11 @@ public class MyAccessibilityService extends AccessibilityService {
             String txt = info.getText().toString();
             txt = txt.toLowerCase();
 
-            // Look for only URL
-            if (txt.contains("yahoo.com")) {
-//                if (txt.contains("http") || txt.contains("www")) {
-                    Log.d(TAG, "dfs: one text is " + txt);
+            // Is the txt a banned URL?
+            if (isPorn(txt)) {
+                    Log.d(TAG, "dfs: the text is " + txt);
 
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("about:blank"));
-                    intent.putExtra(Browser.EXTRA_APPLICATION_ID, "com.android.chrome");
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-
-                      Log.d(TAG, "dfs: Speed = " + (System.currentTimeMillis() - time));
+//                    Log.d(TAG, "dfs: Speed = " + (System.currentTimeMillis() - time));
                     return;
 //                }
             }
@@ -227,14 +179,21 @@ public class MyAccessibilityService extends AccessibilityService {
     public boolean isPorn(String url) {
         // Strip mobile. or m.
         int stop = url.length();
+        url = url.trim();
+
+        if (url.contains(" ") || !url.contains(".")) {
+            return false;
+        }
 
         // Avoid fightthenewdrug and github
         if (!url.contains("fightthenewdrug") && !url.contains("github")) {
+            Log.d(TAG, "isPorn: URL = " + url);
 
             // O(n) worst case feels bad but whO(l)esome porn-checker feels good
             for (int i = 0; i < porNo.pornLinks.length; i++) {
                 if (url.contains(porNo.pornLinks[i])) {
                     // GET THE FUCK OUTTTTTTTTTTTTTTT
+                    Log.d(TAG, "isPorn: TRUE");
 
                     // will this open in the current tab -- yes, but not before the porn site finishes loading smh
                     // about:blank can be thought of as window.stop()
